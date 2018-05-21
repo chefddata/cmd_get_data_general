0993e32d64ca475dcb06b25304fb6a228246f50a
Indrajit Haridas <indrajit@chefd.com>
2018-05-21 09:29:52 -0700

    Update __main__.py

--- get_itemCountPerCustomer/__main__.py
+++ get_itemCountPerCustomer/__main__.py
@@ -5,7 +5,7 @@ import sys
 
 def main():
     print('loading data from BigQuery: Cusomer Shopify ID | Item Shopify ID | Count of Item')
-    query = "SELECT tl_item_id, customer_shopify_id, count(tl_item_id) FROM `tetris-effect-203015.EDW_Netsuite.Transactions` WHERE partition_key > CAST("2015-08-01" AS DATE) group by customer_shopify_id, tl_item_id"
+    query = "SELECT tl_item_id AS item_id, customer_shopify_id AS cust_id, count(tl_item_id) AS count_iterms FROM `tetris-effect-203015.EDW_Netsuite.Transactions` WHERE partition_key > CAST("2015-08-01" AS DATE) group by customer_shopify_id, tl_item_id"
 
     df = gbq.read_gbq(query, project_id = "tetris-effect-203015")
 

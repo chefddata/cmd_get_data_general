554ad0800f8d3c1ccf67c42631b53715555fe16a
Indrajit Haridas <indrajit@chefd.com>
2018-05-21 17:05:58 -0700

    display results on screen

--- get_itemCountPerCustomer/__main__.py
+++ get_itemCountPerCustomer/__main__.py
@@ -1,18 +1,34 @@
 # to communicate with Google BigQuery
-from pandas.io import gbq
-import sys
+import pandas as pd
+from google.cloud import bigquery
+from google.oauth2 import service_account
 
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/indrajitharidas/Downloads/Data Warehouse Production-7bb7eb3af064.json"
+
+credentials = service_account.Credentials.from_service_account_file("/Users/indrajitharidas/Downloads/Data Warehouse Production-7bb7eb3af064.json")
+prj = "tetris-effect-203015"
 
 def main():
+    client = bigquery.Client(credentials= credentials, project = prj)
+    # Set use_legacy_sql to False to use standard SQL syntax.
+    # Note that queries run through the Python Client Library are set to use
+    # standard SQL by default.
+    job_config = bigquery.QueryJobConfig()
+    job_config.use_legacy_sql = False
     print('loading data from BigQuery: Cusomer Shopify ID | Item Shopify ID | Count of Item')
-<<<<<<< HEAD
-    query = "SELECT tl_item_id, customer_shopify_id, count(tl_item_id) FROM `tetris-effect-203015.EDW_Netsuite.Transactions` WHERE partition_key > CAST('2015-08-01' AS DATE) group by customer_shopify_id, tl_item_id"
-=======
-    query = "SELECT tl_item_id AS item_id, customer_shopify_id AS cust_id, count(tl_item_id) AS count_iterms FROM `tetris-effect-203015.EDW_Netsuite.Transactions` WHERE partition_key > CAST("2015-08-01" AS DATE) group by customer_shopify_id, tl_item_id"
->>>>>>> 285ed753403af3dd4fb6bbd4a570771429809ed7
 
-    df = gbq.read_gbq(query, project_id = "tetris-effect-203015")
-    df.to_csv("itemCountPerCustomer.csv")
+    query = "SELECT a.customer_shopify_id, b.shopify_product_id, count(b.shopify_product_id) FROM (SELECT tl_item_id, customer_shopify_id FROM `tetris-effect-203015.EDW_Netsuite.Transactions` WHERE partition_key > CAST('2015-08-01' AS DATE)) a JOIN `tetris-effect-203015.EDW_Netsuite.Items` b ON a.tl_item_id = b.item_id group by a.customer_shopify_id, b.shopify_product_id"
+
+    df = client.query(
+    query,
+    job_config=job_config)  # API request - starts the query
+
+    results = df.result() #wait for job completion
+
+
+    for row in results:
+        print(row)
 
 
 if __name__ == '__main__':
